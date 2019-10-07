1674802ca74741fdd08d9d8ad31af0bb0d11d16b
Ismail El bouhi <iel-bouh@e2r5p3.1337.ma>
2019-10-07 14:04:45 +0100

    start line editing

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: iel-bouh <iel-bouh@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/07/22 15:11:00 by iel-bouh          #+#    #+#              #
-#    Updated: 2019/09/30 14:47:19 by iel-bouh         ###   ########.fr        #
+#    Updated: 2019/10/05 11:39:10 by iel-bouh         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,7 +21,7 @@ all : $(NAME)
 
 $(NAME) : $(OBJ)
 		@make -C libft
-		@$(C) $(CFLAGS) libft/libft.a $(OBJ) -o $(NAME)
+		@$(C) $(CFLAGS) libft/libft.a $(OBJ) -o $(NAME) -ltermcap
 
 %.o	: %.c
 		$(C) $(CFLAGS)  -o $@ -c $<
--- main.c
+++ main.c
@@ -6,14 +6,42 @@
 /*   By: iel-bouh <iel-bouh@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 14:51:08 by iel-bouh          #+#    #+#             */
-/*   Updated: 2019/10/04 13:32:39 by iel-bouh         ###   ########.fr       */
+/*   Updated: 2019/10/07 12:51:31 by iel-bouh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <termios.h>
+#include <curses.h>
+#include <term.h>
+#include <stdlib.h>
 
 int g_ctl;
 
+void	ft_cl_sc(void)
+{
+	char *env;
+	char *str;
+
+	env = getenv("TERM");
+	//errro handling
+	tgetent(NULL, env);
+	str = tgetstr("cl", NULL);
+	ft_putstr(str);
+}
+
+void	ft_key_mode(void)
+{
+	char *env;
+	char *str;
+
+	env = getenv("TERM");
+	//errro handling
+
+	tgetent(NULL, env);
+	str = tgetstr("ks", NULL);
+	ft_putstr(str);
+}
 int		main(int argc, char **argv, char **env)
 {
 	t_env	*key_val;
@@ -21,6 +49,7 @@ int		main(int argc, char **argv, char **env)
 	key_val = NULL;
 	argv = NULL;
 	argc = 0;
+	ft_cl_sc();
 	signal(SIGINT, ft_ctl_c);
 	ft_env_var(env, &key_val);
 	if (!ft_launch(&key_val))
@@ -34,17 +63,56 @@ int		main(int argc, char **argv, char **env)
 	return (0);
 }
 
+static struct termios stored;
+
+void echo_off(void)
+{
+    struct termios new;
+    tcgetattr(0,&stored);
+    memcpy(&new, &stored, sizeof(struct termios));
+    new.c_lflag &= (~ECHO);
+	//new.c_lflag &= (~ICANON);
+	//new.c_cc[VTIME] = 0;
+   // new.c_cc[VMIN] = 1;
+    tcsetattr(0,TCSANOW,&new);
+	g_ctl = 0;
+    return;
+}
+
 int		ft_launch(t_env **key_val)
 {
 	char	*line;
 	int		size;
 	char	*tmp_line;
 
+
+	size = 0;
+
 	while (1)
 	{
 		(g_ctl == 0) ? ft_prompot() : (g_ctl = 1);
 		line = (char *)malloc(sizeof(char) * 1024);
-		size = read(0, line, 1024);
+		//ft_key_mode();
+		tmp_line = line;
+		while (read(0, tmp_line, 1))
+		{
+			if (*tmp_line == EOF || *tmp_line == '\n')
+			{
+				size++;
+				break;
+			}
+			char *env = getenv("TERM");
+			//errro handling
+			tgetent(NULL, env);
+			char *left= tgetstr("kl", NULL);
+			if (ft_strequ(tmp_line, left))
+				ft_putstr(left);
+			if (ft_strequ(tmp_line, "\n"))
+				break;
+			tmp_line++;
+			size++;
+		}
+		//echo_off();
 		line[size] = '\0';
 		tmp_line = ft_strtrim(line);
 		ft_memdel((void **)&line);
